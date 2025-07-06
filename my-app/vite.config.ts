3d849fe487325c5cd24f8ab0d261ef81ee71c76e
Tarsis Lima <br.tmvdl@gmail.com>
2025-07-06 00:59:14 +0000

    Add "5173-tarsislimadev-gitpad-h3bjoqd6qfp.ws-us120.gitpod.io" to `server.allowedHosts` in vite.config.js

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "5173-tarsislimadev-gitpad-h3bjoqd6qfp.ws-us120.gitpod.io"
+    ]
+  }
 })
